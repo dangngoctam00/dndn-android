f9237a21befee58e6c4832a9f9dc9a682b7b0f15
phuongdao8467 <drac19021501bb@gmail.com>
2021-05-14 16:33:57 +0700

    add package controlDevice

--- app/src/main/java/com/example/dadn/di/component/FragmentComponent.java
+++ app/src/main/java/com/example/dadn/di/component/FragmentComponent.java
@@ -3,6 +3,7 @@ package com.example.dadn.di.component;
 
 import com.example.dadn.di.module.FragmentModule;
 import com.example.dadn.di.scope.FragmentScope;
+import com.example.dadn.ui.controlDevice.ControlDeviceFragment;
 import com.example.dadn.ui.device.DeviceFragment;
 import com.example.dadn.ui.home.HomeFragment;
 
@@ -17,4 +18,6 @@ public interface FragmentComponent {
     void inject(HomeFragment fragment);
 
     void inject(DeviceFragment fragment);
+
+    //void inject(ControlDeviceFragment fragment);
 }
---
+++ app/src/main/java/com/example/dadn/ui/controlDevice/ControlDeviceNavigator.java
@@ -0,0 +1,4 @@
+package com.example.dadn.ui.controlDevice;
+
+public interface ControlDeviceNavigator {
+}
