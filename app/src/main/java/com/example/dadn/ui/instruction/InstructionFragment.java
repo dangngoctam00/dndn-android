ea87e1356393fb1c1cd91ea4a400ea304c8a057f
quanledinh <quan.levictoldq38@hcmut.edu.vn>
2021-05-17 02:25:11 +0700

    UI-Instruction fragment: add RecyclerView

--- app/src/main/java/com/example/dadn/ui/instruction/InstructionFragment.java
+++ app/src/main/java/com/example/dadn/ui/instruction/InstructionFragment.java
@@ -3,10 +3,14 @@ package com.example.dadn.ui.instruction;
 
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.dadn.BR;
 import com.example.dadn.R;
@@ -15,10 +19,16 @@ import com.example.dadn.databinding.FragmentInstructionBinding;
 import com.example.dadn.di.component.FragmentComponent;
 import com.example.dadn.ui.base.BaseFragment;
 
+import java.util.ArrayList;
+
 
 public class InstructionFragment extends BaseFragment<FragmentInstructionBinding, InstructionViewModel> implements InstructionNavigator {
 
+    private RecyclerView mRecyclerView;
+    private InstructionAdapter mAdapter;
     FragmentInstructionBinding mFragmentInstructionBinding;
+    private ArrayList<InstructionItem> instructionList;
+
 
     @Override
     public int getBindingVariable() {
@@ -39,6 +49,25 @@ public class InstructionFragment extends BaseFragment<FragmentInstructionBinding
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mViewModel.setNavigator(this);
+
+
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView= inflater.inflate(R.layout.fragment_instruction, container, false);
+        mRecyclerView = rootView.findViewById(R.id.recyclerView_InstructionsFrag);
+        mRecyclerView.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        mAdapter = new InstructionAdapter(getContext());
+
+        instructionList.add(new InstructionItem(R.drawable.user, "Title", "Details"));
+        instructionList.add(new InstructionItem(R.drawable.user, "Title", "Details"));
+
+        mAdapter.setData(instructionList);
+        mRecyclerView.setLayoutManager(linearLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
+        return rootView;
     }
 
     @Override
