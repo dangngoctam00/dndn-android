dac8406f5927b3c9176a84324149d599e0c722b9
quanledinh <quan.levictoldq38@hcmut.edu.vn>
2021-06-10 00:11:48 +0700

    update fcm

--- app/src/main/java/com/example/dadn/ui/setting/SettingViewModel.java
+++ app/src/main/java/com/example/dadn/ui/setting/SettingViewModel.java
@@ -1,8 +1,10 @@
 package com.example.dadn.ui.setting;
 
 import com.example.dadn.ui.base.BaseViewModel;
+import com.example.dadn.ui.controlDevice.selectDevice.SelectDeviceFragment;
 import com.example.dadn.ui.setting.fragment.HelpFragment;
 import com.example.dadn.ui.setting.fragment.IntroductionFragment;
+import com.example.dadn.ui.setting.fragment.Setting2Fragment;
 import com.example.dadn.utils.rx.SchedulerProvider;
 
 public class SettingViewModel extends BaseViewModel<SettingNavigator> {
@@ -14,6 +16,9 @@ public class SettingViewModel extends BaseViewModel<SettingNavigator> {
     }
     public void openHelpFragment(){
         getNavigator().ReplaceFragment(new HelpFragment());
+    }
+    public void openSetting2Fragment(){
+        getNavigator().ReplaceFragment(new Setting2Fragment());
     }
 
 }
--- app/src/main/java/com/example/dadn/utils/PreferenceUtilities.java
+++ app/src/main/java/com/example/dadn/utils/PreferenceUtilities.java
@@ -8,6 +8,7 @@ public class PreferenceUtilities {
     public static final String KEY_ALERT = "Alert";
     public static final String KEY_IS_ALERT_PROCESSING = "isAlertProcessing";
     public static final String KEY_CAN_NOT_HANDLE = "cannotHandle";
+    public static final String KEY_ALERT_ACTIVE = "alertactive";
     private static final Boolean DEFAULT_BOOLEAN = false;
 
     synchronized public static void SetAlertState(Context context, Boolean i) {
@@ -42,6 +43,18 @@ public class PreferenceUtilities {
     public static Boolean getcannotHandle(Context context) {
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
         Boolean i = prefs.getBoolean(KEY_CAN_NOT_HANDLE, DEFAULT_BOOLEAN);
+        return i;
+    }
+
+    synchronized public static void SetAlertActive(Context context, Boolean i) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putBoolean(KEY_ALERT_ACTIVE, i);
+        editor.apply();
+    }
+    public static Boolean getAlertActive(Context context) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        Boolean i = prefs.getBoolean(KEY_ALERT_ACTIVE, true);
         return i;
     }
 
