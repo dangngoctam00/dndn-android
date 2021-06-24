8b331c0f2a380f34d7cb75974e266f46e716365a
dangngoctam00 <dangngoctam00@gmail.com>
2021-06-24 15:24:32 +0700

    fix type of api response

--- app/src/main/java/com/example/dadn/data/dto/UpdateSpecificationResponse.java
+++ app/src/main/java/com/example/dadn/data/dto/UpdateSpecificationResponse.java
@@ -5,8 +5,18 @@ import com.google.gson.annotations.SerializedName;
 
 public class UpdateSpecificationResponse {
     @Expose
+    @SerializedName("code")
+    private int code;
+
+    @Expose
     @SerializedName("message")
     private String message;
+
+
+    public int getCode() {
+        return code;
+    }
+
 
     public String getMessage() {
         return message;
